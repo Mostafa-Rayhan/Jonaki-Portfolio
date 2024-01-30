fce9aec890b99bbf22cece54f8e82a0e2107247c
Mostafa Rayhan <mostafarayhan57@gmail.com>
2024-01-31 03:57:01 +0600

    initial push

---
+++ src/components/Navbar/navbar.js
@@ -0,0 +1,23 @@
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import React from 'react'
+
+const navbar = () => {
+  return (
+          <Navbar bg="light" data-bs-theme="light">
+            <Container>
+                <Navbar.Brand href="#home">Navbar</Navbar.Brand>
+                    <Nav className="mx-auto">
+                        <Nav.Link href="#home">Home</Nav.Link>
+                        <Nav.Link href="#About">About</Nav.Link>
+                        <Nav.Link href="#services">Services</Nav.Link>
+                        <Nav.Link href="#Contact">Contact</Nav.Link>
+                        <Nav.Link href="#Blog">Blog</Nav.Link>
+                </Nav>
+            </Container>
+        </Navbar>
+  )
+}
+
+export default navbar
