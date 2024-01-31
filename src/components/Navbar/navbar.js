7aea4c8364277ba4a177fbaac9e59b84dd20bf74
Mostafa Rayhan <mostafarayhan57@gmail.com>
2024-02-01 05:29:53 +0600

    navbar responsive

--- src/components/Navbar/navbar.js
+++ src/components/Navbar/navbar.js
@@ -4,26 +4,29 @@ import { NavLink } from "react-router-dom";
 
 const navbar = () => {
   return (
-    <Navbar bg="light" data-bs-theme="light">
+    <Navbar expand="lg" bg="light" data-bs-theme="light">
       <Container>
         <Navbar.Brand href="home">Jonaki Khanam</Navbar.Brand>
-        <Nav className="mx-auto">
-          <Nav.Link as={NavLink} to="/">
-            Home
-          </Nav.Link>
-          <Nav.Link as={NavLink} to="/about">
-            About
-          </Nav.Link>
-          <Nav.Link as={NavLink} to="/services">
-            Services
-          </Nav.Link>
-          <Nav.Link as={NavLink} to="/contact">
-            Contact
-          </Nav.Link>
-          <Nav.Link as={NavLink} to="/blog">
-            Blog
-          </Nav.Link>
-        </Nav>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="mx-auto">
+            <Nav.Link as={NavLink} to="/">
+              Home
+            </Nav.Link>
+            <Nav.Link as={NavLink} to="/about">
+              About
+            </Nav.Link>
+            <Nav.Link as={NavLink} to="/services">
+              Services
+            </Nav.Link>
+            <Nav.Link as={NavLink} to="/contact">
+              Contact
+            </Nav.Link>
+            <Nav.Link as={NavLink} to="/blog">
+              Blog
+            </Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
       </Container>
     </Navbar>
   );
