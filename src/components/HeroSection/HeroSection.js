8b07b05637fe69bd5ff648763f085a6fdf0d88bb
Mostafa Rayhan <mostafarayhan57@gmail.com>
2024-02-10 03:36:26 +0600

    hero section done

--- src/components/HeroSection/HeroSection.js
+++ src/components/HeroSection/HeroSection.js
@@ -2,32 +2,38 @@ import React from 'react'
 import {Bio} from "../../data/constant"
 import Typewriter from 'typewriter-effect';
 import Button from 'react-bootstrap/Button';
-
+import '../../styles/HomePage.css'
+import Image from 'react-bootstrap/Image';
 
 const HeroSection = () => {
 
     
   return (
     <div className='bg-white'>
-        <div className='container'>
+        <div className='container sectionGap'>
             <div className='row'>
-                <div className='col-sm-12 col-md-6 mt-5'>
-                    <h6>Hi, I am <br /> {Bio.name}</h6>
-                    <div>
-                        I am a
-                        <Typewriter
-                            options={{
-                                strings: Bio.roles,
-                                autoStart: true,
-                                loop: true,
-                            }}
-                        />
+                <div className='col-sm-12 col-md-6'>
+                    <h6 className='header1 fw-bold'>Hi, I am <br /> {Bio.name}</h6>
+                    <div className='typing d-flex fw-bold'>
+                        I am a 
+                        <span className='typingSpan mx-2'>
+                            <Typewriter
+                                options={{
+                                    strings: Bio.roles,
+                                    autoStart: true,
+                                    loop: true,
+                                }}
+                            />
+                        </span>
+                        
                     </div>
-                    <p>{Bio.description}</p>
-                    <Button variant="primary">Check Resume</Button>
+                    <p className='peragraph'>{Bio.description}</p>
+                    <Button variant="primary" size="lg" className=''>Check Resume</Button>
                 </div>
                 
-                <div className='col-sm-12 col-md-6'>img</div>
+                <div className='col-sm-12 col-md-6 text-center'>
+                    <Image src="https://i.ibb.co/sjpHqBz/jonaki-khanam.png" className='' />
+                </div>
             </div>
         </div>
         
