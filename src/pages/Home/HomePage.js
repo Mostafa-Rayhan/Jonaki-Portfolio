f11480ca148c76e76012dfe72ef3f10d7e4d039c
Mostafa Rayhan <mostafarayhan57@gmail.com>
2024-02-15 03:28:34 +0600

    Footer

--- src/pages/Home/HomePage.js
+++ src/pages/Home/HomePage.js
@@ -1,12 +1,14 @@
 import React from 'react'
 import HeroSection from '../../components/HeroSection/HeroSection'
 import Expertise from '../../components/Expertise/Expertise'
+import Footer from '../../components/Footer/Footer'
 
 const Home = () => {
   return (
       <div>
         <HeroSection />
         <Expertise />
+        <Footer />
       </div>   
   )
 }
