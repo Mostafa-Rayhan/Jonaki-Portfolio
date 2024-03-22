934eadfb7d6ddc1f0c2632796298513a5f23ef3e
Mostafa Rayhan <mostafarayhan57@gmail.com>
2024-03-23 01:41:01 +0600

    Certifications

---
+++ src/components/Certification/Certification.js
@@ -0,0 +1,55 @@
+import React from 'react'
+
+import Card from 'react-bootstrap/Card';
+import { Certifications } from '../../data/constant';
+// import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import Image from 'react-bootstrap/Image';
+
+const Certification = () => {
+  return (
+    <div className='sectionGap pb-5'>
+        {/* <Container> */}
+            <div className='text-center'>
+                <h1 className='header1'>Certifications</h1>
+                <p className='peragraph'>I have worked on a wide range of projects. Here are some of my projects.</p>
+            </div>
+
+            <div className='mt-5'>
+                <Row xs={1} md={4} className="g-4 px-5">
+                    {Certifications.map((Certification, index) => (
+                        <Col key={Certification.id}>
+                            <Card className='border-0 shadow h-100'>
+                                {/* <Card.Body className='card-hover'> */}
+                                    <div className='rounded-3 p-2'>
+                                        <Image
+                                            src={Certification?.img}                                           
+                                            height={300}
+                                            // width={180}
+                                            rounded
+                                            fluid 
+                                            className=''
+                                        />
+                                    </div>
+                                    <Card.Title className='p-3'>
+                                        {Certification.title}
+                                    </Card.Title>
+                                    {/* <Card.Text>
+                                        {expert.desc}
+                                    </Card.Text> */}
+                                {/* </Card.Body> */}
+                            </Card>
+                        </Col>
+                    ))}  
+                </Row>   
+            </div>
+            
+        {/* </Container> */}
+        
+        
+    </div>
+  )
+}
+
+export default Certification
--- src/pages/About/About.js
+++ src/pages/About/About.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import AboutMe from '../../components/AboutMe/AboutMe'
+import Certification from '../../components/Certification/Certification'
 
 const About = () => {
   return (
     <div>
       <AboutMe />
+      <Certification />
     </div>
   )
 }
