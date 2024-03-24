225ed16ffb288ce99a074565627ed7a185ed84da
Mostafa Rayhan <mostafarayhan57@gmail.com>
2024-03-25 00:03:21 +0600

    about update

--- src/pages/About/About.js
+++ src/pages/About/About.js
@@ -1,12 +1,14 @@
 import React from 'react'
 import AboutMe from '../../components/AboutMe/AboutMe'
 import Certification from '../../components/Certification/Certification'
+import Contact from '../../components/Contact/Contact'
 
 const About = () => {
   return (
     <div>
       <AboutMe />
       <Certification />
+      <Contact />
     </div>
   )
 }
